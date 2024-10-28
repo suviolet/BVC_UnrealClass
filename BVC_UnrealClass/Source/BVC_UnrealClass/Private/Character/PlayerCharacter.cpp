5b066a4bc7907db350040ead2d660940684fee96
Suellen S. <suellendesalles@gmail.com>
2024-10-28 13:51:34 -0600

    Class3: Character Movement

--- BVC_UnrealClass/Source/BVC_UnrealClass/Private/Character/PlayerCharacter.cpp
+++ BVC_UnrealClass/Source/BVC_UnrealClass/Private/Character/PlayerCharacter.cpp
@@ -3,13 +3,22 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 APlayerCharacter::APlayerCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
 	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->TargetArmLength = 600.f;
 
 	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
 	Camera->SetupAttachment(CameraBoom);
@@ -26,14 +35,18 @@ void APlayerCharacter::BeginPlay()
 		{
 			Subsystem->AddMappingContext(InputMappingContext, 0);
 		}
+
+		PlayerController->PlayerCameraManager->ViewPitchMax = 15.f;
+		PlayerController->PlayerCameraManager->ViewPitchMin = -45.f;
 	}
 }
 
-void APlayerCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
-}
+//void APlayerCharacter::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
 
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -43,5 +56,34 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &APlayerCharacter::Jump);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Movement);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Started, this, &APlayerCharacter::Look);
 	}
+}
+
+void APlayerCharacter::Movement(const FInputActionValue& Value)
+{
+	//GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Emerald, FString::Printf(TEXT("X: %f / Y: %f"), MovementVector.X, MovementVector.Y));
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	AddMovementInput(RightDirection, MovementVector.X);
+
+	//AddControllerYawInput(MovementVector.X);
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	//GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red, FString::Printf(TEXT("X: %f / Y: %f"), LookVector.X, LookVector.Y));
+	
+	const FVector2D LookVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookVector.X);
+	AddControllerPitchInput(LookVector.Y);
 }
--- BVC_UnrealClass/Source/BVC_UnrealClass/Public/Character/PlayerCharacter.h
+++ BVC_UnrealClass/Source/BVC_UnrealClass/Public/Character/PlayerCharacter.h
@@ -3,12 +3,15 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 //#include "Camera/CameraComponent.h" //not necessary here
+//#include "InputActionValue.h" //not necessary here
 #include "PlayerCharacter.generated.h"
 
+class UCameraComponent;
 class UCameraComponent;
 class USpringArmComponent;
 class UInputMappingContext;
 class UInputAction;
+struct FInputActionValue;
 
 UCLASS()
 class BVC_UNREALCLASS_API APlayerCharacter : public ACharacter
@@ -17,19 +20,31 @@ class BVC_UNREALCLASS_API APlayerCharacter : public ACharacter
 
 public:
 	APlayerCharacter();
-	virtual void Tick(float DeltaTime) override;
+	//virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	virtual void BeginPlay() override;
 
+	void Movement(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+	/*
+		Inputs
+	*/
 	UPROPERTY(EditAnywhere, Category = Input)
 	UInputMappingContext* InputMappingContext;
 
 	UPROPERTY(EditAnywhere, Category = Input)
 	//UInputAction* JumpAction; //old type of pointer
-	TObjectPtr <UInputAction> JumpAction; //new type of pointer
+	TObjectPtr <UInputAction> JumpAction; //TObjectPtr new type of pointer
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr <UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr <UInputAction> MoveAction;
 
 private:
 	UPROPERTY(EditAnywhere, Category = Camera)
